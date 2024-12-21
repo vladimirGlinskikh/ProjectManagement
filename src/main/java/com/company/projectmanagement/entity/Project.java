8d8f151d63c981ecaa8e151b976855b6a48d9cea
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-12-21 15:29:27 +0300

    JPA Entityes

---
+++ src/main/java/com/company/projectmanagement/entity/Project.java
@@ -0,0 +1,71 @@
+package com.company.projectmanagement.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "PROJECT", indexes = {
+        @Index(name = "IDX_PROJECT_MANAGER", columnList = "MANAGER_ID")
+})
+@Entity
+public class Project {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+    @InstanceName
+    @Column(name = "NAME", nullable = false)
+    @NotNull
+    private String name;
+    @JoinColumn(name = "MANAGER_ID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User manager;
+    @Column(name = "DESCRIPTION")
+    @Lob
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getManager() {
+        return manager;
+    }
+
+    public void setManager(User manager) {
+        this.manager = manager;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
